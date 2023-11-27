20b7bf6375b573ddee7ff3a64aee3a16490f415d
GabrielMarconatto-terra <gabriel.marconatto_compasso@terrainvestimentos.com.br>
2023-11-27 18:15:14 -0300

    first commit

---
+++ src/main/java/com/example/SpringBootJava_01/Domain/StatusTopico.java
@@ -0,0 +1,10 @@
+package com.example.SpringBootJava_01.Domain;
+
+public enum StatusTopico {
+	
+	NAO_RESPONDIDO,
+	NAO_SOLUCIONADO,
+	SOLUCIONADO,
+	FECHADO;
+
+}
---
+++ src/main/java/com/example/SpringBootJava_01/SpringBootJava01Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootJava_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJava01Application
+{
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJava01Application.class, args);
+	}
+
+}
