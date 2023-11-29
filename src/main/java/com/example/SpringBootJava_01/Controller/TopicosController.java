bbfb3559ca90bfe02a850866470eabd848fc6efc
GabrielMarconatto-terra <gabriel.marconatto_compasso@terrainvestimentos.com.br>
2023-11-29 13:49:10 -0300

    feat: add cache no post

--- src/main/java/com/example/SpringBootJava_01/Controller/TopicosController.java
+++ src/main/java/com/example/SpringBootJava_01/Controller/TopicosController.java
@@ -9,6 +9,7 @@ import com.example.SpringBootJava_01.JpaRepository.CursoRepository;
 import com.example.SpringBootJava_01.JpaRepository.TopicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -49,6 +50,7 @@ public class TopicosController
 
     @PostMapping
     @Transactional
+    @CacheEvict(value = "listaDeTopicos", allEntries = true)
     public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder)
     {
         Topico topico = form.converter(cursoRepository);
