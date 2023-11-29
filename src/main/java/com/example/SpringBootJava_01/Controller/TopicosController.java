e59aa0dbf814e9f19b5e2e37e7a4f611fdcc58f0
GabrielMarconatto-terra <gabriel.marconatto_compasso@terrainvestimentos.com.br>
2023-11-29 11:28:23 -0300

    feat: criado delete

--- src/main/java/com/example/SpringBootJava_01/Controller/TopicosController.java
+++ src/main/java/com/example/SpringBootJava_01/Controller/TopicosController.java
@@ -42,6 +42,7 @@ public class TopicosController
     }
 
     @PostMapping
+    @Transactional
     public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder)
     {
         Topico topico = form.converter(cursoRepository);
@@ -64,5 +65,13 @@ public class TopicosController
     {
         Topico topico = form.atualizar(id, topicoRepository);
         return ResponseEntity.ok(new TopicoDto(topico));
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity<?> remover(@PathVariable Long id)
+    {
+        topicoRepository.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 }
